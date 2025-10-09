4649973506edb04a0c793dfcc5c7d61611b0df34
Svatopluk Krejsa <krejsas@oadomazlice.com>
2025-10-09 11:33:37 +0200

    pridani gitu a delani stranky

---
+++ js/script.js
@@ -0,0 +1,8 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+let smoother = ScrollSmoother.create({
+    wrapper: '#smooth-wrapper',
+    content: '#smooth-content',
+    smooth: 1,
+    effects: true
+})
