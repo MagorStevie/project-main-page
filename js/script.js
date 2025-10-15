49ce3cd02038316eea9f0a786494ddbb9eae7c4f
MagorStevie <svatikrejsa2008@gmail.com>
2025-10-15 19:10:08 +0200

    dodelani html a posledni dodelani gsapu

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
 let smoother = ScrollSmoother.create({
     wrapper: '#smooth-wrapper',
     content: '#smooth-content',
     smooth: 1,
     effects: true
-})
+});
 
 let button = document.querySelector(".home_button")
 
 button.addEventListener("click", (e) => {
     smoother.scrollTo("h1", true, "center, center")
-})
+});
+
+
+let split = SplitText.create(".text", {type: "chars ,words, lines"});
+
+gsap.from(split.chars, {
+    duration: 1.4,
+    y:100,
+    autoAlpha:0,
+    stragger: 0.05
+});
